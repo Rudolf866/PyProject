bab2544de1985b7e0bd6286456ac406f17ea1676
Rudolf <kovalevskiy2017@outlook.com>
2020-07-04 15:15:33 +0300

    Create connect db

--- settings/settings.py
+++ settings/settings.py
@@ -10,10 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
 
+import json
+import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# from django.core.serializers import json
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+try:
+    with open(os.path.join(BASE_DIR, 'conf', 'secrets.json')) as handle:
+        SECRETS = json.load(handle)
+except IOError:
+    SECRETS = {
+        'secret_key': 'a'
+    }
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,6 +39,8 @@ SECRET_KEY = 'j=li!gc8v(bn^f+6q@_$^zu8zfq=zxzipkpzun!y(0sbyn=csr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+# DEBUG = False необходимо добавить домен сайта
 ALLOWED_HOSTS = []
 
 
@@ -37,6 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # пользовательские приложения
+    'common',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +73,7 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,12 +92,29 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'pyproject',
+#         'USER':'python',
+#         'HOST':'localhost',
+#         'PASSWORD':'qaz1986qaz',
+#         'PORT':'3306',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': SECRETS.get('db_name', ''),
+        'USER': SECRETS.get('db_user', ''),
+        'HOST': SECRETS.get('db_host', ''),
+        'PASSWORD': SECRETS.get('db_password', ''),
+        'PORT': SECRETS.get('db_port', ''),
+    },
 }
+
 
 
 # Password validation
@@ -103,9 +139,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -118,3 +154,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
